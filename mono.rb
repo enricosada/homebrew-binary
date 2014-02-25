ecae0dd552bd87a895ee730870025129c717ce97
Enrico Sada <enrico@sada.io>
2014-02-25 03:00:32 +0100

    download only, no unzip

--- mono.rb
+++ mono.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Mono < Formula
   homepage 'http://www.mono-project.com/'
 
-  url "http://download.xamarin.com/MonoFrameworkMDK/Macx86/MonoFramework-MDK-3.2.6.macos10.xamarin.x86.pkg"
+  url "http://download.xamarin.com/MonoFrameworkMDK/Macx86/MonoFramework-MDK-3.2.6.macos10.xamarin.x86.pkg", :using => :nounzip
   sha1 'f3f8eca1232989327240bbb6a7a000250003c33e'
 
   def install
